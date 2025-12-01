31e9adc539534521317444456ef1219a178b84b3
Swayam Patwa <swayampatwaxyz@gmail.com>
2025-12-01 12:42:48 +0530

    Delete food-order directory

--- food-order/admin/partials/footer.php
+++
@@ -1,4 +0,0 @@
- <!-- Footer  -->
- <div class="footer text-center">
-        <p>&copy; 2025 WowFood, All rights reserved, Developed by ~ <span class='developer'>Swayam Patwa</span></p>
-    </div>
--- food-order/partials-front/footer.php
+++
@@ -1,5 +0,0 @@
-<section class="footer">
-    <div class="container text-center">
-      <p>&copy; 2025 WowFood, All rights reserved. Order your <a href="#"> favorite food </a> now! </p>
-    </div>
-</section>
